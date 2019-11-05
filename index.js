1bfdc6bdf2718d13271a976503c2564dc61aff24
Adam Finch <atyreefinch@gmail.com>
2019-11-05 15:42:22 -0500

    lots of stuff

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 
+// https://poly.googleapis.com/v1/assets?keywords=spaceship&format=OBJ&key=AIzaSyBC9puPjTcS63cU_HLfcSqOqs3Lrx7QXvk
+
+//https://cors-anywhere.herokuapp.com/
 
 const API_KEY = 'AIzaSyBC9puPjTcS63cU_HLfcSqOqs3Lrx7QXvk';
 
 function init () {
     document.querySelector("#polyQuery").addEventListener("submit",searchPoly)
     document.querySelector("#forward-button").addEventListener("click",nextObj)
-    document.querySelector("#back-button").addEventListener("click",prevObj)
+    let backward = document.querySelector("#back-button")
+    backward.addEventListener("click",prevObj)
+    backward.disabled = true;
     document.querySelector("#bigger-button").addEventListener("click",biggify)
     document.querySelector("#smaller-button").addEventListener("click",smallify)
 }
 
 document.addEventListener("DOMContentLoaded",init);
 
+let keywords = "Search Me"
+
 function searchPoly( e ) {
     e.preventDefault()
-    let keywords = e.target[0].value
+    keywords = e.target[0].value
     e.target.reset()
-    fetch(`https://poly.googleapis.com/v1/assets?keywords=${keywords}&format=OBJ&key=${API_KEY}`)
+    fetch(`https://poly.googleapis.com/v1/assets?keywords=${keywords}&format=OBJ&key=${API_KEY}`,
+    {header: {'Access-Control-Allow-Origin':'*',}})
         .then(response => response.json())
         .then(saveObjResourcesLocally)
 }
@@ -24,19 +32,41 @@ function searchPoly( e ) {
 let objHolder = [];
 let objInt = 0;
 let scale = 1;
+let pageToken = false;
 
 function saveObjResourcesLocally(searchObjs) {
     objHolder = [];
-    searchObjs.assets.forEach(searchObj => { 
+    searchObjs.assets.forEach(pusher)
+    if (searchObjs.nextPageToken) {
+    pageToken = searchObjs.nextPageToken
+    } else {
+        pageToken = false;
+    }
+    renderCurrentObj(objHolder);
+}
+
+function pusher(searchObj) {
+    let relevantIndex = indexFinder(searchObj);
+    if (searchObj.formats[relevantIndex].resources) {
     objHolder.push({
         displayName: `${searchObj.displayName}`,
         authorName: `${searchObj.authorName}`,
-        rootUrl:`${searchObj.formats[0].root.url}`,
-        mtl: `${searchObj.formats[0].resources[0].url}`,
+        rootUrl:`${searchObj.formats[relevantIndex].root.url}`,
+        mtl: `${searchObj.formats[relevantIndex].resources[0].url}`,
         thumbnail : `${searchObj.thumbnail.url}`
         })
-    })
-    renderCurrentObj(objHolder);
+    } else {
+        objHolder.push({
+            displayName: `${searchObj.displayName}`,
+            authorName: `${searchObj.authorName}`,
+            rootUrl:`${searchObj.formats[relevantIndex].root.url}`,
+            thumbnail : `${searchObj.thumbnail.url}`
+        })
+    }
+}
+
+function indexFinder(searchObj) {
+    return searchObj.formats.findIndex(format => format.formatType === "OBJ");
 }
 
 function renderCurrentObj(objArr) {
@@ -58,7 +88,8 @@ function renderCurrentObj(objArr) {
     objEntity.setAttribute('scale',`${scale} ${scale} ${scale}`);
     stage.append(objEntity);
     document.getElementById("bottom-ui").classList.remove("hidden")
-    document.getElementById("model-name").innerText = ` ${displayItem.displayName} by ${displayItem.authorName} `
+    document.getElementById("model-name").innerText = ` ${displayItem.displayName} `
+    document.getElementById("model-by").innerText = `Model by: ${displayItem.authorName} `
 }
 
 function destroyCurrentObj() {
@@ -79,12 +110,24 @@ function destroyCurrentObj() {
 }
 
 function nextObj(e) {
+    let objLen = objInt+1
+    if (objLen === objHolder.length && pageToken)
+    {
+        loadMoreObjects()
+    } else if (objLen === objHolder.length) {
+        alert("There's no more objects for this search!")
+    } else {
     objInt++
+    document.querySelector("#back-button").disabled = false;
     renderCurrentObj(objHolder);
+    }
 }
 
 function prevObj(e) {
     objInt--
+    if (objInt === 0) {
+        e.currentTarget.disabled = true;
+    }
     renderCurrentObj(objHolder);
 }
 
@@ -96,4 +139,12 @@ function biggify(e) {
 function smallify(e) {
     document.querySelector("#current-entity").setAttribute('scale',`${scale*.9} ${scale*.9} ${scale*.9}`);
     scale = scale*.9
+}
+
+function loadMoreObjects(){
+    objInt = 0;
+    let newUrl = `https://poly.googleapis.com/v1/assets?keywords=${keywords}&format=OBJ&key=${API_KEY}&pageToken=${pageToken}`
+    fetch(newUrl)
+        .then(response => response.json())
+        .then(saveObjResourcesLocally)
 }
